b5697d7c3d311c2d48c91f152092a264c48fb5ba
Daria Khanina <khaninadaria@gmail.com>
2023-09-19 22:48:36 +0300

    fix: fixed incorrect path handling

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use((req, res) => {
+  res.status(404).json({ message: 'Запрашиваемый ресурс не найден' });
+});
+
 app.get('/', (req, res) => {
   res.json({ success: true });
 });
