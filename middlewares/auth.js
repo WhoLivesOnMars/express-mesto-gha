46018e25581c79fa6fbeaad694e81aac8455e627
Daria Khanina <khaninadaria@gmail.com>
2023-10-07 01:05:49 +0300

    fix: made changes after tests

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,17 @@
 const jwt = require('jsonwebtoken');
-const { HTTP_STATUS_UNAUTHORIZED } = require('node:http2').constants;
+const UnauthorizedError = require('../errors/unauthorized-err');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(HTTP_STATUS_UNAUTHORIZED)
-      .send({ message: 'Ошибка авторизации' });
-  }
-
-  const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
   let payload;
 
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
-    return res
-      .status(HTTP_STATUS_UNAUTHORIZED)
-      .send({ message: 'Ошибка авторизации' });
+    next(new UnauthorizedError('Требуется авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
 
-  return next(); // пропускаем запрос дальше
+  next(); // пропускаем запрос дальше
 };
